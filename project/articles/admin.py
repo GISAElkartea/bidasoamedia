a9bb3d4f126bf390beefdfd8cc316632f03d6acd
Unai Zalakain <contact@unaizalakain.info>
2012-10-25 00:23:59 +0200

    auto_now_add makes editable=False

--- project/articles/admin.py
+++ project/articles/admin.py
@@ -6,10 +6,16 @@ from project.articles.models import Article, Category
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ('name',)
 admin.site.register(Category, CategoryAdmin)
 
 
 class ArticleAdmin(FilePickerAdmin):
+    date_hierarchy = 'pub_date'
+    fields = (('title', 'pub_date'), 'categories', 'description', 'body')
+    filter_horizontal = ('categories',)
+    list_display = ('title', 'pub_date')
+    list_filter = ('categories', 'pub_date')
+    search_fields = ('title', 'description', 'body')
     adminfiles_fields = ['description', 'body']
 admin.site.register(Article, ArticleAdmin)
--- project/articles/models.py
+++ project/articles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import ugettext as _
+from django.utils.timezone import now
 
 from markitup.fields import MarkupField
 from autoslug import AutoSlugField
@@ -20,17 +21,19 @@ class Category(models.Model):
 
 class Article(models.Model):
     class Meta:
+        ordering = ('-pub_date',)
         verbose_name = _('Article')
         verbose_name_plural = _('Articles')
 
     title = models.CharField(max_length=100, verbose_name=_('title'))
-    description = MarkupField(blank=True, verbose_name=_('description'))
+    description = MarkupField(blank=True, verbose_name=_('description'), 
+            help_text=_('populated from body if not given'))
     body = MarkupField(verbose_name=_('body'))
 
-    pub_date = models.DateTimeField(auto_now_add=True,
+    pub_date = models.DateTimeField(default=now,
             verbose_name=_('publication date'))
-    category = models.ManyToManyField(Category, blank=True, null=True,
-            verbose_name=_('category'))
+    categories = models.ManyToManyField(Category, blank=True, null=True,
+            verbose_name=_('categories'))
     slug = AutoSlugField(populate_from='title', unique=True)
 
     def __unicode__(self):
