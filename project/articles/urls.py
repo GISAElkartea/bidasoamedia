3af75ba4026e5e1cebafe82e87b7f99213ca704b
Unai Zalakain <contact@unaizalakain.info>
2012-10-25 10:50:26 +0200

    article url

--- project/articles/urls.py
+++ project/articles/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
         url('^$', ArticleList.as_view(), name='list'),
         url('^category/(?P<slug>(\w|\d|-)+)/$', ArticleCategory.as_view(), 
             name='category'),
-        url('^(?P<slug>(\w|\d|-)+)/$', ArticleDetail.as_view(), name='detail'),
+        url('^article/(?P<slug>(\w|\d|-)+)/$', ArticleDetail.as_view(), 
+            name='detail'),
 )
