0c386fa508370f49569ebb5dc4737c2cfcf91c81
Unai Zalakain <contact@unaizalakain.info>
2012-10-25 11:38:17 +0200

    load all urls starting with 'articles' with ajax

---
+++ project/assets/js/bidasoamedia.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('body a').live('click', function(event) {
+        link = $(this).attr('href');
+
+        if (link.indexOf('/articles/') == 0) {
+            event.preventDefault();
+            $.ajax({url: link}).success(function(data) {
+                $('section#articles').html(data);
+            });
+        };
+    });
+});
