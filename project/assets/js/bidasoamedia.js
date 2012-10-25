d49f88d8818aa419df56cd1575c9a1af686a4453
Unai Zalakain <contact@unaizalakain.info>
2012-10-25 12:05:08 +0200

    remove & append instead of trying to replace

--- project/assets/js/bidasoamedia.js
+++ project/assets/js/bidasoamedia.js
@@ -5,7 +5,8 @@ $(function() {
         if (link.indexOf('/articles/') == 0) {
             event.preventDefault();
             $.ajax({url: link}).success(function(data) {
-                $('section#articles').html(data);
+                $('section#articles *').remove()
+                $('section#articles').append(data);
             });
         };
     });
