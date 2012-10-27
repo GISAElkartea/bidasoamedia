28a5f0973af5d961d95e8cbe7699174b06c33525
Unai Zalakain <contact@unaizalakain.info>
2012-10-27 21:32:12 +0200

    return feed object list as feed_list context variable

--- project/feeds/context_processors.py
+++ project/feeds/context_processors.py
@@ -1,7 +1,4 @@
-from itertools import chain
-
 from project.feeds.models import Feed
 
 def feed_list(request):
-    feeds = chain(( feed.parse() for feed in Feed.objects.iterator() ))
-    return {'feed_list': feeds}
+    return {'feed_list': Feed.objects.iterator()}
