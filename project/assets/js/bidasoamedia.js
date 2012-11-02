7bdcad79ff330c84e851003d80549b09b1e3b54b
Unai Zalakain <contact@unaizalakain.info>
2012-11-02 16:49:01 +0100

    scroll feeds

--- project/assets/js/bidasoamedia.js
+++ project/assets/js/bidasoamedia.js
@@ -1,4 +1,4 @@
-$(function() {
+function ajax() {
     $('body a').live('click', function(event) {
         link = $(this).attr('href');
 
@@ -9,4 +9,18 @@ $(function() {
             });
         };
     });
+};
+
+function scroll_feeds() {
+    setTimeout(function() {
+        var first = $('#feeds article:first');
+        first.remove();
+        $('#feeds').append(first);
+        scroll_feeds();
+    }, 5000);
+};
+
+$(function() {
+    ajax();
+    scroll_feeds();
 });
