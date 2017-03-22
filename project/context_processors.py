e26d057e598d4170d43b8e5b6a0a581685840b6f
Unai Zalakain <unai@gisa-elkartea.org>
2017-03-22 22:28:46 +0000

    Do not use yammy for templates anymore

--- project/context_processors.py
+++ project/context_processors.py
@@ -1,5 +1,5 @@
 def ajax_template(request):
     if request.is_ajax():
-        return {'base': 'ajax_base.yammy'}
+        return {'base': 'ajax_base.html'}
     else:
-        return {'base': 'base.yammy' }
+        return {'base': 'base.html' }
